3690f0ed54abdb51fae4cc6c4003678439a72a03
Ben Nathanson <benedwinnathanson@gmail.com>
2022-09-05 15:34:56 -0400

    Add test stubs

---
+++ test/logic/test_holiday_engine.py
@@ -0,0 +1,18 @@
+def test_get_cached_country_holidays_handles_cache_miss():
+    ...
+
+
+def test_get_cached_country_holidays_handles_cache_hit():
+    ...
+
+
+def test_get_holiday_name_returns_expected_response():
+    ...
+
+
+def test_get_supported_countries_returns_expected_response():
+    ...
+
+
+def test_is_holiday_returns_expected_response():
+    ...
