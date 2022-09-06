397ed3725b8be02be9f915a12c35e11eef93f6ba
Ben Nathanson <benedwinnathanson@gmail.com>
2022-09-05 22:48:24 -0400

    test_get_holiday_name_returns_expected_response

--- test/logic/test_holiday_engine.py
+++ test/logic/test_holiday_engine.py
@@ -2,19 +2,28 @@ import datetime as dt
 
 import pytest
 
-from src.logic import holiday_engine
+from src.logic import HolidayEngine, holiday_engine
 
 
 def test_get_cached_country_holidays_handles_cache_miss():
-    ...
+    # need to ensure a cold start
+    engine_instance = HolidayEngine()
+    assert engine_instance.is_holiday("US", "2022-07-04") is True
 
 
 def test_get_cached_country_holidays_handles_cache_hit():
-    ...
+    # need to ensure a cold start
+    engine_instance = HolidayEngine()
+    assert engine_instance.is_holiday("US", "2022-07-04") is True
+    assert engine_instance.is_holiday("US", "2022-07-04") is True
 
 
-def test_get_holiday_name_returns_expected_response():
-    ...
+@pytest.mark.parametrize(
+    "date,holiday_name",
+    [("2022-07-04", "Independence Day"), ("2022-09-05", "Labor Day")],
+)
+def test_get_holiday_name_returns_expected_response(date: str, holiday_name: str):
+    assert holiday_engine.get_holiday_name("US", date) == holiday_name
 
 
 def test_get_supported_countries_returns_expected_country_detail():
@@ -41,4 +50,4 @@ def test_cached_supported_countries_included_expected_countries():
 )
 def test_is_holiday_returns_expected_response(date: dt.date, expected_result: bool):
     holiday_decision: bool = holiday_engine.is_holiday("US", date)
-    assert holiday_decision == expected_result
+    assert holiday_decision is expected_result
