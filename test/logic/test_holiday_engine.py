b974254ec24636100492ecf3de4133737a48d3b8
Ben Nathanson <benedwinnathanson@gmail.com>
2022-09-05 22:33:49 -0400

    Add tests for get_supported_countries

--- test/logic/test_holiday_engine.py
+++ test/logic/test_holiday_engine.py
@@ -13,8 +13,17 @@ def test_get_holiday_name_returns_expected_response():
     ...
 
 
-def test_get_supported_countries_returns_expected_response():
-    ...
+def test_get_supported_countries_returns_expected_country_detail():
+    supported_countries = holiday_engine.get_supported_countries()
+    united_states, *_ = [c for c in supported_countries if c.alpha_2 == "US"]
+    assert united_states.flag == "🇺🇸"
+    assert united_states.name == "United States"
+
+
+def test_get_supported_countries_returns_expected_countries():
+    supported_countries = holiday_engine.get_supported_countries()
+    supported_countries_set = {c.alpha_2 for c in supported_countries}
+    assert {"GB", "MX", "US"}.intersection(supported_countries_set)
 
 
 def test_cached_supported_countries_included_expected_countries():
