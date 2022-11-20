8087ae82ba3380902a67c65bb76bdad68b50b7f0
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-19 20:32:50 -0500

    Add basic email/password based Oauth2 via Firebase

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from os import environ
 
+from fastapi import FastAPI
+from firebase_admin import App, initialize_app
+
+from src.view.routers.account_management import account_management_router
 from src.view.routers.holiday import holiday_router
 
+FIREBASE_CREDENTIALS_PATH = "src/firebase-credentials.json"
+environ["GOOGLE_APPLICATION_CREDENTIALS"] = FIREBASE_CREDENTIALS_PATH
+
+
+firebase_app: App = initialize_app()
 app: FastAPI = FastAPI(title="Forget Me Nots")
 app.include_router(holiday_router)
+app.include_router(account_management_router)
---
+++ src/view/routers/account_management.py
@@ -0,0 +1,48 @@
+import json
+
+import requests
+from fastapi import APIRouter
+from firebase_admin import auth
+from requests import Response
+
+import src.view.models as view_models
+
+account_management_router = APIRouter(prefix="/users", tags=["users"])
+
+with open("src/firebase-credentials.json") as credentials_file:
+    CREDENTIALS = json.load(credentials_file)
+API_KEY = CREDENTIALS["api_key"]
+
+
+@account_management_router.post(
+    "/create", response_model=view_models.CreateUserResponse
+)
+def create_user(payload: view_models.CreateUserPayload):
+    user_record: auth.UserRecord = auth.create_user(
+        email=payload.email, password=payload.password
+    )
+    return view_models.CreateUserResponse(email=user_record.email)
+
+
+@account_management_router.post("/login", response_model=view_models.LoginResponse)
+def login(payload: view_models.LoginPayload):
+    url: str = (
+        f"https://www.googleapis.com/identitytoolkit/v3/relyingparty"
+        f"/verifyPassword?key={API_KEY}"
+    )
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    request_body = json.dumps(
+        {
+            "email": payload.email,
+            "password": payload.password,
+            "returnSecureToken": True,
+        }
+    )
+    response: Response = requests.post(url, headers=headers, data=request_body)
+    response_json: dict = response.json()
+    return view_models.LoginResponse(
+        email=response_json["email"],
+        expires_in=response_json["expiresIn"],
+        id_token=response_json["idToken"],
+        access_token=response_json["refreshToken"],
+    )
