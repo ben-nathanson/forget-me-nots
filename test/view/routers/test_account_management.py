9b18dde5160e62f69054f3b1f122fec65079ade7
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-19 23:48:33 -0500

    Swallow UserNotFoundError in tests.

--- test/view/routers/test_account_management.py
+++ test/view/routers/test_account_management.py
@@ -5,7 +5,7 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 from firebase_admin import auth
-from firebase_admin.auth import UserRecord
+from firebase_admin.auth import UserNotFoundError, UserRecord
 from requests import Response  # type: ignore
 
 from src.main import app
@@ -22,9 +22,11 @@ class AccountManagementFixture(unittest.TestCase):
         self.client: TestClient = TestClient(app)
 
     def tearDown(self):  # noqa
-        user: UserRecord | None = auth.get_user_by_email(self.email_address)
-        if user:
+        try:
+            user: UserRecord = auth.get_user_by_email(self.email_address)
             auth.delete_user(user.uid)
+        except UserNotFoundError:
+            pass
 
 
 class TestCreateUser(AccountManagementFixture):
@@ -65,3 +67,11 @@ class TestLogin(AccountManagementFixture):
         assert response_json["email"] == self.email_address
         assert len(response_json["idToken"])
         assert len(response_json["accessToken"])
+
+    # def test_that_we_cant_login_with_wrong_password(self):
+    #     for password in [self.password[::-1], self.password[1:], self.password[:-1], secrets.token_urlsafe(15)]:
+    #         with self.subTest():
+    #             raw_payload: dict = {"email": self.email_address, "password": password}
+    #             response: Response = self.client.post(self.login_route,
+    #                                                   json=raw_payload)
+    #             assert not response.ok
