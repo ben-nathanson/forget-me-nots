3a571f90bd8c2a4d14e8ad21fc78e94531c27a3c
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-19 23:10:34 -0500

    Add a simple test fixture/test that creates a user and cleans up after itself.

---
+++ test/view/routers/test_account_management.py
@@ -0,0 +1,34 @@
+import secrets
+import unittest
+import uuid
+
+from fastapi.testclient import TestClient
+from firebase_admin import auth
+from firebase_admin.auth import UserRecord
+from requests import Response  # type: ignore
+
+from src.main import app
+
+
+class AccountManagementFixture(unittest.TestCase):
+    random_guid = str(uuid.uuid4())
+    email_address: str = f"ben+automatedtesting+{random_guid}@nathanson.dev"
+    password: str = secrets.token_urlsafe(15)
+
+    def setUp(self):  # noqa
+        self.client: TestClient = TestClient(app)
+
+    def tearDown(self):  # noqa
+        user: UserRecord | None = auth.get_user_by_email(self.email_address)
+        if user:
+            auth.delete_user(user.uid)
+
+
+class TestCreateUser(AccountManagementFixture):
+    route: str = "users/create"
+
+    def test_that_we_can_create_a_user(self):
+        raw_payload: dict = {"email": self.email_address, "password": self.password}
+        response: Response = self.client.post(self.route, json=raw_payload)
+        assert response.ok
+        assert response.json()["email"] == self.email_address
