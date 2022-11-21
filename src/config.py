6171b3a7850449bc3e6758183fc7e6d96b57b210
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-20 22:21:47 -0500

    Test CredentialManager

--- src/config.py
+++ src/config.py
@@ -1,30 +1,37 @@
 import json
 import os
+from typing import Optional
 
 from firebase_admin.credentials import Certificate
 
 
 class CredentialManager:
     _raw_firebase_credentials: dict
-    firebase_credentials_path: str = "src/firebase-credentials.json"
-    firebase_environment_key: str = "SECRET_FIREBASE_CREDENTIALS"
-    firebase_cert: Certificate
-    firebase_api_key: str
+    _firebase_credentials_path: str
+    _firebase_environment_key: str
 
-    def __init__(self):
-        if os.path.exists(self.firebase_credentials_path):
+    def __init__(
+        self,
+        firebase_credentials_path: Optional[str] = None,
+        firebase_environment_key: Optional[str] = None,
+    ):
+        self._firebase_credentials_path = (
+            firebase_credentials_path or "src/firebase-credentials.json"
+        )
+        self._firebase_environment_key = (
+            firebase_environment_key or "SECRET_FIREBASE_CREDENTIALS"
+        )
+        if os.path.exists(self._firebase_credentials_path):
             self._raw_firebase_credentials: dict = json.load(
-                open(self.firebase_credentials_path)
+                open(self._firebase_credentials_path)
             )
         else:
             self._raw_firebase_credentials: dict = json.loads(
-                os.environ.get(self.firebase_environment_key)
+                os.environ.get(self._firebase_environment_key)
             )
-        self.firebase_cert = Certificate(self._raw_firebase_credentials)
-        self.firebase_api_key = self._raw_firebase_credentials["api_key"]
 
     def get_firebase_api_key(self) -> str:
-        return self.firebase_api_key
+        return self._raw_firebase_credentials["api_key"]
 
     def get_firebase_cert(self) -> Certificate:
-        return self.firebase_cert
+        return Certificate(self._raw_firebase_credentials)
---
+++ test/test_config.py
@@ -0,0 +1,49 @@
+import json
+import os
+import unittest
+
+from src.config import CredentialManager
+
+MOCK_CREDENTIALS: dict = {
+    "api_key": "",
+    "auth_provider_x509_cert_url": "www.test.test",
+    "auth_uri": "www.test.test",
+    "client_email": "test@nathanson.dev",
+    "client_id": "123456789123456789123",
+    "client_x509_cert_url": "www.test.test",
+    "private_key": "",
+    "private_key_id": "test-key-id",
+    "project_id": "test-project-id-123",
+    "token_uri": "www.test.test",
+    "type": "service_account",
+}
+
+
+class TestCredentialManager(unittest.TestCase):
+    firebase_credentials_path: str = "test/test_credentials.json"
+    firebase_environment_key: str = "TEST_CREDENTIALS"
+
+    def tearDown(self):
+        if os.path.exists(self.firebase_credentials_path):
+            os.remove(self.firebase_credentials_path)
+        os.environ.pop(self.firebase_credentials_path, None)
+
+    def test_can_get_credentials_from_file(self):
+        with open(self.firebase_credentials_path, "w+") as credentials_file:
+            credentials_file.write(json.dumps(MOCK_CREDENTIALS))
+
+        credential_manager: CredentialManager = CredentialManager(
+            firebase_credentials_path=self.firebase_credentials_path,
+            firebase_environment_key=self.firebase_environment_key,
+        )
+
+        assert credential_manager._raw_firebase_credentials == MOCK_CREDENTIALS
+
+    def test_can_get_credentials_from_env_variable(self):
+        os.environ[self.firebase_environment_key] = json.dumps(MOCK_CREDENTIALS)
+        credential_manager: CredentialManager = CredentialManager(
+            firebase_credentials_path=self.firebase_credentials_path,
+            firebase_environment_key=self.firebase_environment_key,
+        )
+
+        assert credential_manager._raw_firebase_credentials == MOCK_CREDENTIALS
