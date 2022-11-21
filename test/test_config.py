6e5f27fcb9b4112e1cf2bb59be55b6facd2a0a95
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-20 22:31:55 -0500

    Cleanup and nosec for whenever security tooling is added

--- test/test_config.py
+++ test/test_config.py
@@ -5,13 +5,13 @@ import unittest
 from src.config import CredentialManager
 
 MOCK_CREDENTIALS: dict = {
-    "api_key": "",
+    "api_key": "test_api_key",  # nosec
     "auth_provider_x509_cert_url": "www.test.test",
     "auth_uri": "www.test.test",
     "client_email": "test@nathanson.dev",
     "client_id": "123456789123456789123",
     "client_x509_cert_url": "www.test.test",
-    "private_key": "",
+    "private_key": "test_private_key",  # nosec
     "private_key_id": "test-key-id",
     "project_id": "test-project-id-123",
     "token_uri": "www.test.test",
@@ -28,10 +28,8 @@ class TestCredentialManager(unittest.TestCase):
             os.remove(self.firebase_credentials_path)
         os.environ.pop(self.firebase_credentials_path, None)
 
-    def test_can_get_credentials_from_file(self):
-        with open(self.firebase_credentials_path, "w") as credentials_file:
-            credentials_file.write(json.dumps(MOCK_CREDENTIALS))
-
+    def test_can_get_credentials_from_env_variable(self):
+        os.environ[self.firebase_environment_key] = json.dumps(MOCK_CREDENTIALS)
         credential_manager: CredentialManager = CredentialManager(
             firebase_credentials_path=self.firebase_credentials_path,
             firebase_environment_key=self.firebase_environment_key,
@@ -39,8 +37,10 @@ class TestCredentialManager(unittest.TestCase):
 
         assert credential_manager._raw_firebase_credentials == MOCK_CREDENTIALS
 
-    def test_can_get_credentials_from_env_variable(self):
-        os.environ[self.firebase_environment_key] = json.dumps(MOCK_CREDENTIALS)
+    def test_can_get_credentials_from_file(self):
+        with open(self.firebase_credentials_path, "w") as credentials_file:
+            credentials_file.write(json.dumps(MOCK_CREDENTIALS))
+
         credential_manager: CredentialManager = CredentialManager(
             firebase_credentials_path=self.firebase_credentials_path,
             firebase_environment_key=self.firebase_environment_key,
