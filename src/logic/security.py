c88b91dd6b9a2e166ab7dabbde4ff3140178c8af
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-25 13:01:09 -0500

    Add password checking logic

---
+++ src/logic/security.py
@@ -0,0 +1,26 @@
+import re
+
+
+def is_strong_password(password: str) -> bool:
+    if not isinstance(password, str):
+        return False
+
+    contains_lowercase: bool = password.upper() != password
+    contains_uppercase: bool = password.lower() != password
+    contains_numbers: bool = any(
+        [character for character in password if character.isnumeric()]
+    )
+    contains_special_characters: bool = any(
+        [character for character in password if not re.match("[A-Za-z0-9]", character)]
+    )
+    is_longer_than_six_characters = len(password) >= 6
+    if (
+        contains_lowercase
+        and contains_uppercase
+        and contains_numbers
+        and contains_special_characters
+        and is_longer_than_six_characters
+    ):
+        return True
+    else:
+        return False
---
+++ test/logic/test_security.py
@@ -0,0 +1,28 @@
+import unittest
+
+from src.logic.security import is_strong_password
+
+
+class TestPasswordChecker(unittest.TestCase):
+    def test_rejects_bad_passwords(self):
+        bad_passwords = [
+            123,
+            "",
+            "password",
+            "lks~!",
+            "lowercase",
+            "UPPERCASE",
+            "NoSpecialCharacters",
+            "NoNumbers!",
+            "cH^qmmjSj",
+            "2cH88qmmjSj",
+        ]
+        for password in bad_passwords:
+            with self.subTest():
+                assert not is_strong_password(password)
+
+    def test_accepts_good_passwords(self):
+        good_passwords = ["2cH88^qmmjSj", "7S$u37M8M^kF", "wK27*rv7@$Jx"]
+        for password in good_passwords:
+            with self.subTest():
+                assert is_strong_password(password)
