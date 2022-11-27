fa7bb9990482c382e06718874b8f81fea3d6c48e
Ben Nathanson <benedwinnathanson@gmail.com>
2022-11-27 10:54:56 -0500

    Add a test to check that we can create a valid token

--- test/view/routers/test_account_management.py
+++ test/view/routers/test_account_management.py
@@ -16,9 +16,11 @@ from src.logic.services.account_management import (
 from src.main import app
 
 
-class AccountManagementFixture(unittest.TestCase):
+class AccountManagementBaseFixture(unittest.TestCase):
     create_route: str = "users/create"
     login_route: str = "users/login"
+    create_token_route: str = "/users/token"
+    validate_token_route: str = "/users/validate-oauth-token"
     random_guid = str(uuid.uuid4())
     email_address: str = f"ben+automatedtesting+{random_guid}@nathanson.dev"
     password: str = generate_strong_password()
@@ -34,7 +36,14 @@ class AccountManagementFixture(unittest.TestCase):
             pass
 
 
-class TestCreateUser(AccountManagementFixture):
+class CreateAccountFixture(AccountManagementBaseFixture):
+    def setUp(self):
+        super().setUp()
+        raw_payload: dict = {"email": self.email_address, "password": self.password}
+        self.client.post(self.create_route, json=raw_payload)
+
+
+class TestCreateUser(AccountManagementBaseFixture):
     def test_that_we_can_create_a_user(self):
         raw_payload: dict = {"email": self.email_address, "password": self.password}
         response: Response = self.client.post(self.create_route, json=raw_payload)
@@ -73,12 +82,7 @@ class TestCreateUser(AccountManagementFixture):
                 assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-class TestLogin(AccountManagementFixture):
-    def setUp(self):
-        super().setUp()
-        raw_payload: dict = {"email": self.email_address, "password": self.password}
-        self.client.post(self.create_route, json=raw_payload)
-
+class TestLogin(CreateAccountFixture):
     def test_that_we_can_login(self):
         raw_payload: dict = {"email": self.email_address, "password": self.password}
         response: Response = self.client.post(self.login_route, json=raw_payload)
@@ -106,6 +110,23 @@ class TestLogin(AccountManagementFixture):
                 assert response.status_code == HTTPStatus.FORBIDDEN
 
 
+class TestSwaggerOpenApiLogin(CreateAccountFixture):
+    def test_that_we_can_create_a_valid_token(self):
+        request_form = {"username": self.email_address, "password": self.password}
+        create_token_response: Response = self.client.post(
+            self.create_token_route, request_form
+        )
+        assert create_token_response.ok, create_token_response.status_code
+        access_token: str = create_token_response.json()["access_token"]
+        headers: dict = {"Authorization": f"Bearer {access_token}"}
+        validate_token_response: Response = self.client.get(
+            self.validate_token_route, headers=headers
+        )
+        assert validate_token_response.ok, validate_token_response.status_code
+        assert validate_token_response.json()["token"] == access_token
+
+
+# TODO this doesn't really belong in the view layer tests; it makes no requests.
 class TestPasswordChecker(unittest.TestCase):
     def test_rejects_bad_passwords(self):
         bad_passwords = [
